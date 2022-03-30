484a5612b11ba8838e82fe2c6d7748dc65952047
An Yonghee <ayh0429@gmail.com>
2022-03-30 17:35:51 +0900

    extra add gitignore

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -28,7 +28,7 @@ const AppLayout = ({children}) => {
                 <Col xs={24} md={12} >
                 {children}
                 </Col>``
-                <Col xs={24} md={6} >dddd
+                <Col xs={24} md={6} >
                 오른쪽 메뉴
                 </Col>
             </Row>
